97da23004d2acede102cc90fe29d30f7d4d3b303
ivank <ivan.kuleshin@gmail.com>
2023-01-08 14:22:01 +0100

    Update checkstyle configuration

--- src/test/java/utils/JSONFileReader.java
+++ src/test/java/utils/JSONFileReader.java
@@ -11,7 +11,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 public class JSONFileReader {
     static ObjectMapper objectMapper = new ObjectMapper();
@@ -19,14 +18,6 @@ public class JSONFileReader {
     public static Product readJSONFile(String path) {
         try {
             return objectMapper.readValue(new File(path), Product.class);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static String JSOnFileToString(String path) {
-        try {
-            return Files.readString(Path.of(path));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -40,46 +31,11 @@ public class JSONFileReader {
         }
     }
 
-    public static Employee changeCity(String path, String change, int number) {
-        Employee employee = readJSONFileEmployee(path);
-        employee.getAddresses().get(number).setCity(change);
-        return employee;
-    }
-
-    public static Employee changePhoneEmployee(Employee employee, String[] newValue) {
-        employee.setPhones(newValue);
-        return employee;
-    }
-
-    public static Employee changeValue(Employee employee, String path, String newValue) {
-        String[] pathValue = path.split("[.\\[\\]]");
-        int num = path.indexOf("[");
-        int index = Character.getNumericValue(path.charAt(num + 1));
-        switch (pathValue[pathValue.length - 1]) {
-            case "name" -> {
-                if ((pathValue.length - 1) <= 1) {
-                    employee.setName(newValue);
-                    break;
-                }
-                employee.getCompany().setName(newValue);
-            }
-            case "position" -> employee.setPosition(newValue);
-            case "city" -> employee.getAddresses().get(index).setCity(newValue);
-            case "country" -> employee.getAddresses().get(index).setCountry(newValue);
-            case "id" -> employee.getCompany().setId(newValue);
+    public static String JSOnFileToString(String path) {
+        try {
+            return Files.readString(Path.of(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-        return employee;
-    }
-
-
-    public static Employee changeAddressEmployee(Employee employee, List<Address> newValue) {
-        employee.setAddresses(newValue);
-        return employee;
-    }
-
-    public static Employee changeCompanyEmployee(Employee employee, Company newValue) {
-        employee.setCompany(newValue);
-        return employee;
     }
 }
--- src/test/java/utils/UpdateJsonHelper.java
+++ src/test/java/utils/UpdateJsonHelper.java
@@ -13,23 +13,18 @@ import static com.google.common.collect.Iterables.getLast;
 import static com.google.common.collect.Iterables.limit;
 import static utils.JSONFileReader.JSOnFileToString;
 
-public class JsonHelper {
+public class UpdateJsonHelper {
   private static final String PATH_SEPARATOR_REGEXP = "\\.";
   private static final String PATH_SEPARATOR = ".";
 
-  public static <T> T updateFieldsByPath(
-      String jsonPath, String fullPath, Object valueToUpdate, Class<T> classToCast) {
-    DocumentContext documentContext = JsonPath.parse(JSOnFileToString(jsonPath));
+  public static <T> T updateFieldByPath(
+      String pathToJsonFile, String fullFieldPath, Object valueToUpdate, Class<T> classToCast) {
+    DocumentContext documentContext = JsonPath.parse(JSOnFileToString(pathToJsonFile));
 
-    List<String> pathLevels = Arrays.asList(fullPath.split(PATH_SEPARATOR_REGEXP));
+    List<String> pathLevels = Arrays.asList(fullFieldPath.split(PATH_SEPARATOR_REGEXP));
     String path = String.join(PATH_SEPARATOR, limit(pathLevels, pathLevels.size() - 1));
     String fieldToUpdate = getLast(pathLevels);
-    Object field = documentContext.read(fullPath);
-    if (field instanceof List) {
-      documentContext.put(path, fieldToUpdate, valueToUpdate);
-    } else if (field instanceof String) {
-      documentContext.put(path, fieldToUpdate, valueToUpdate);
-    }
+    documentContext.put(path, fieldToUpdate, valueToUpdate);
 
     try {
       return new ObjectMapper().readValue(documentContext.jsonString(), classToCast);
